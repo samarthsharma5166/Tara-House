092cf56e7cb625dde8b0f7dffbeb15989b37db2f
samarth sharma <samarths716@gmail.com>
2025-07-09 08:52:52 +0530

    api bug

--- tara/helper/axiosInstance.ts
+++ tara/helper/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
